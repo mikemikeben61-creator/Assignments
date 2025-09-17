1b74084035873b7a4a80e22838ebdd9f60618388
mike <mikemikeben61@gmail.com>
2025-09-17 20:29:05 +0300

    Add files via upload

---
+++ mike program.c
@@ -0,0 +1,18 @@
+//simple c program(Hello world)
+/*
+Name:NZISA MICHAEL MUSYOKA
+Reg no:CT100/G/26226/25
+Description:Displaying units of semester one year one
+*/
+#include <stdio.h>
+int main(){
+    printf("Analogue electronics\n");
+    printf("Mathematics for science\n");
+    printf("Introduction to programing and Algorithms\n");
+    printf("Fundamentals of ICT\n");
+    printf("Computer systems and Organizations\n");
+    printf("Essentials of accounting\n");
+    printf("Introduction to computer applications\n");
+    printf("Communication and information literacy skills\n");
+    return 0;
+}
